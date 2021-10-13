5eddff0891a13132cd4d60c63f813265b9f3ed67
John Stef <johnstef1999@gmail.com>
2021-10-14 02:57:59 +0300

    ⚡ Enable format on save

--- lua/config.lua
+++ lua/config.lua
@@ -42,6 +42,9 @@ local on_attach = function(client, bufnr)
 	buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
 	buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
+  if client.resolved_capabilities.document_formatting then
+    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+  end
 end
 --}}}
 
@@ -152,6 +155,7 @@ lsp.html.setup{
   on_attach = on_attach,
   capabilities = capabilities,
 }
+vim.cmd("autocmd BufWritePre *.html lua vim.lsp.buf.formatting_sync()")
 --}}}
 
 -- json {{{
