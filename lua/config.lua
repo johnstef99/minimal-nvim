39ceb89022ed1435c2efcef043d402dbcb4b0546
John Stef <johnstef1999@gmail.com>
2021-10-14 02:57:07 +0300

    ⚡ Define missing functions

--- lua/config.lua
+++ lua/config.lua
@@ -55,6 +55,15 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = cmp_lsp.update_capabilities(capabilities)
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 cmp.setup({
     snippet = {
       expand = function(args)
