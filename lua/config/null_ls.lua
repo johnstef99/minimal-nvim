6eb13fcb20b7044917c41247820edda1e92fd430
johnstef99 <john@johnstef.com>
2022-01-19 21:12:01 +0200

    config null_ls

--- lua/config/null_ls.lua
+++ lua/config/null_ls.lua
@@ -1,18 +1,13 @@
 local M = {}
 
 local null_ls = require("null-ls")
-local lsp = require("lspconfig")
-
 local on_attach = require("config.lsp.on_attach")
 local sources = {
 	null_ls.builtins.formatting.stylua,
 }
 
 function M.init()
-	null_ls.config({ sources = sources })
-	lsp["null-ls"].setup({
-		on_attach = on_attach,
-	})
+	null_ls.setup({ sources = sources, on_attach = on_attach })
 end
 
 return M
