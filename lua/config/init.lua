afe8d927dd0ab24dcbf405f65ce5b3dfd7093da6
johnstef99 <john@johnstef.com>
2022-01-22 23:14:25 +0200

    🔭 ignore pdf files in telescope

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -3,3 +3,8 @@ require("config.cmp").init()
 require("config.null_ls").init()
 require("config.lualine").init()
 require("config.tree_sitter").init()
+require("telescope").setup{
+  defaults = {
+    file_ignore_patterns = {".*.pdf"}
+  }
+}
