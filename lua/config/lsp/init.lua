ede4d2c855832a3304609a62638212049029c281
johnstef99 <john@johnstef.com>
2022-06-17 21:32:20 +0300

    update config for sumneko_lua

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -117,20 +117,9 @@ local function languages()
 	--}}}
 
 	-- lua {{{
-	local system_name
-	if vim.fn.has("mac") == 1 then
-		system_name = "macOS"
-	elseif vim.fn.has("unix") == 1 then
-		system_name = "Linux"
-	elseif vim.fn.has("win32") == 1 then
-		system_name = "Windows"
-	else
-		print("Unsupported system for sumneko")
-	end
-
 	-- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
 	local sumneko_root_path = vim.fn.stdpath("data") .. "/lsp_servers/sumneko_lua/extension/server"
-	local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name .. "/lua-language-server"
+	local sumneko_binary = sumneko_root_path .. "/bin/lua-language-server"
 
 	local runtime_path = vim.split(package.path, ";")
 	table.insert(runtime_path, "lua/?.lua")
--- lua/config/null_ls.lua
+++ lua/config/null_ls.lua
@@ -2,12 +2,10 @@ local M = {}
 
 local null_ls = require("null-ls")
 local on_attach = require("config.lsp.on_attach")
-local sources = {
-	null_ls.builtins.formatting.stylua,
-}
+local sources = {}
 
 function M.init()
-	null_ls.setup({ sources = sources, on_attach = on_attach })
+  null_ls.setup({ sources = sources, on_attach = on_attach })
 end
 
 return M
