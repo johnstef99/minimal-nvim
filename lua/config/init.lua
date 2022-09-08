290b23c406d6fc6b2d11cf767fc6b975e90009e7
johnstef99 <john@johnstef.com>
2022-09-08 15:02:57 +0300

    replace tabs with spaces

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -5,14 +5,14 @@ require("config.lualine").init()
 require("config.tree_sitter").init()
 require("config.barbar").init()
 require("telescope").setup({
-	defaults = {
-		file_ignore_patterns = { ".*.pdf" },
-	},
-	extensions = {
-		["ui-select"] = {
-			require("telescope.themes").get_dropdown(),
-		},
-	},
+  defaults = {
+    file_ignore_patterns = { ".*.pdf" },
+  },
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown(),
+    },
+  },
 })
 require("telescope").load_extension("ui-select")
 require("fidget").setup()
