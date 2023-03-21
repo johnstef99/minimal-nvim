96dee86d37b48a71f97efb9a6562a47044d285fe
johnstef99 <john@johnstef.com>
2023-03-21 20:43:22 +0200

    bugfix unpack

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -11,7 +11,8 @@ end
 -- M.capabilities = capabilities
 
 local has_words_before = function()
-	local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+	---@diagnostic disable-next-line: deprecated
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
